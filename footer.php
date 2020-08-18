4c788dcd0ae21b146f7ea9538d2b774c2a058447
Pranto Kumar <prantok104@gmail.com>
2020-08-18 22:13:16 +0600

    Footer file

---
+++ footer.php
@@ -0,0 +1,44 @@
+
+    <!--jQuery-->
+    <script src="assets/js/jquery-1.10.2.min.js"></script>
+
+    <!--Syotimer JS-->
+    <script src="assets/js/plugins/jquery.syotimer.min.js"></script>
+
+    <!--Bootstrap JS-->
+    <script src="assets/js/plugins/bootstrap.min.js"></script>
+
+    <!--Waypoint JS-->
+    <script src="assets/js/plugins/waypoints.min.js"></script>
+
+    <!--Counterup JS-->
+    <script src="assets/js/plugins//jquery.counterup.min.js"></script>
+
+    <!--Owl Carousel JS-->
+    <script src="assets/js/plugins/owl.carousel.min.js"></script>
+
+    <!--Isotop JS -->
+    <script src="assets/js/plugins/isotope.min.js"></script>
+
+    <!--Slick-Nav JS -->
+    <script src="assets/js/plugins/jquery.slicknav.min.js"></script>
+
+    <!--Magnific JS -->
+    <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+
+    <!--Chart JS -->
+    <script src="assets/js/plugins/chart.min.js"></script>
+    
+    <!--Wow JS -->
+    <script src="assets/js/plugins/wow.min.js"></script>
+
+    <!--Custom JS (Add your custom JS scripts to this file)-->
+    <script src="assets/js/custom.js"></script>
+    <!--Wow Active Js-->
+    <script>
+        new WOW().init();
+    </script>
+
+</body>
+
+</html>
