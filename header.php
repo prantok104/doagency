befc4aa63079f1eb6fc97916254f583caac6bfc5
Pranto Kumar <prantok104@gmail.com>
2020-08-18 22:11:06 +0600

    header file

---
+++ header.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!--Page Title-->
+    <title> Eco - Construction and Multipurpose HTML5 Template</title>
+
+    <!--Favicons-->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/image/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/image/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/image/favicons/favicon-16x16.png">
+    <link rel="manifest" href="assets/image/favicons/site.webmanifest">
+
+    <!--Fontawesome CSS-->
+    <link rel="stylesheet" href="assets/css/plugins/all.min.css">
+
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+
+    <!--Owl-Carousel CSS-->
+    <link rel="stylesheet" href="assets/css/plugins/owl.carousel.min.css">
+
+    <!--Owl-theme CSS-->
+    <link rel="stylesheet" href="assets/css/plugins/owl.theme.default.min.css">
+
+    <!--Animate CSS-->
+    <link rel="stylesheet" href="assets/css/plugins/animate.min.css">
+
+    <!--Slick-Nav CSS-->
+    <link rel="stylesheet" href="assets/css/plugins/slicknav.min.css">
+
+    <!--Magnific CSS-->
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+
+    <!--Normalize CSS-->
+    <link rel="stylesheet" href="assets/css/plugins/normalize.css">
+
+    <!--Style CSS (Add your custom CSS style to this file)-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--Responsive CSS (Add your custom responsive CSS style to this file)-->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <!--Pre-Loader area start-->
+    <div class="loader-bg1">
+        <div class="loader1"></div>
+    </div>
+    <!--Pre-Loader area end-->
+
+    <!--header area start-->
+    <header class="header-area-strat shadow-lg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-xl-3 col-md-3">
+                    <div class="logo-area">
+                        <a href="index.php"><span>do</span>agency</a>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-xl-7 col-md-7">
+                    <nav class="main-menu">
+                        <ul class="primary-menu">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">service</a></li>
+                            <li><a href="#">portfolio</a></li>
+                            <li><a href="#">blog</a></li>
+                            <li><a href="#">pages</a>
+                                <ul class="drop-menu">
+                                    <li><a href="404.php">404 Page</a></li>
+                                    <li><a href="comming-soon.php">comming-soon</a></li>
+                                    <li><a href="comming-soon.php">partners</a></li>
+                                    <li><a href="comming-soon.php">FAQ</a></li>
+                                    <li><a href="comming-soon.php">our team</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-lg-2 col-xl-2 col-md-2">
+                    <div class="search-area">
+                        <a href="#" data-target="#search" data-toggle="modal"><span class="fas fa-search"></span></a>
+                        <div class="modal fade" id="search" tabindex="-1">
+                            <div class="modal-dialog">
+                                <div class="modal-content df-bg">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Search Here</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <input type="text" name="s" id="s" placeholder="Search for...">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--header area end-->
